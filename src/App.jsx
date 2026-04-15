77cb53bc547ee8fdea4a8b6ea41c5e5f37a29313
lud <ludamare3@gmail.com>
2026-04-15 19:30:46 +0300

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
